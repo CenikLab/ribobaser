d829018b49fbeb8a3e987b92382183655cf64dc3
Shilpa0206 <shilparao@utexas.edu>
2025-12-03 11:48:54 -0600

    Add method for zero imputation using GBM

--- Multiplicative_Imputation.R
+++ Multiplicative_Imputation.R
@@ -1,11 +1,13 @@
 # to implement multiplicative imputation
 # the method deletes columns /rows , genes /samples which have >80% unobserved or zero values. 
+## For Multiplicative to work, samples should be in rows and genes should be in columns
+# Ensure genes are rownames instead of a separate column as is the case when importing a csv file 
 
-
+library(zCompositions)
 Imputation <- function(dt1,
                        method = "GBM",
                        output = "p-count",
-                       transpose = TRUE,
+                       transpose = TRUE
                        ) {
   
   if (!is.numeric(dt1[,1])) {
@@ -14,7 +16,7 @@ Imputation <- function(dt1,
     rownames(dt1) <- gene_names
   } else {
     gene_names <- rownames(dt1)
-  }
+  }   
   
   # Optionally transpose input data
   if (transpose) {
